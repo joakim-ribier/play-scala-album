bded71a1d88bbc68fc90ed18274507719adb9e88
Joakim Ribier <joakim.ribier@gmail.com>
2012-12-19 21:49:40 +0600

    fix special character in http url

--- gallery/app/utils/TokenUtils.scala
+++ gallery/app/utils/TokenUtils.scala
@@ -5,6 +5,6 @@ object TokenUtils {
   def validationAddressMail(username: String, addressMail: String) : String = {
     val str = username + Configuration.getToken() + addressMail
     val md = java.security.MessageDigest.getInstance("SHA-1")
-    return new sun.misc.BASE64Encoder().encode(md.digest(str.getBytes))
+    return new sun.misc.BASE64Encoder().encode(md.digest(str.getBytes)).replaceAll("/", "")
   }
 }
